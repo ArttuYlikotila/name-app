8b3517b3e3482c1d20b6524d225ca0986369ce15
Arttu Ylikotila <arttu.ylikotila@gmail.com>
2020-12-20 20:53:29 +0200

    Fixes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 'use strict'
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8000;
 const api = require('./routes/router');
 
 app.use('/api/', api);
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+}
+else {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+}
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
